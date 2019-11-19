1c9d4a4dc907fe6272233be070b88fafb3a26c0b
Kuran <kurankumar@gmail.com>
2019-11-19 09:59:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400 ) {
+    let response = "This one is on me!";
+    return response;
+    
+  } 
+
+  else if ( distance >= 2001 && distance < 2500) {
+
+    let response = "I will gladly take your thirty bucks.";
+    return response;
+
+  }
+  else {
+    let response = "No can do.";
+    return response;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city == "NYC") {
+    return "Ok, sounds good.";
+    
+  } 
+  else {
+    return "No go.";
+    
+  }
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip;
+
+function switchOnCharmFromTip(tip){
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+
+      case 'not as generous':
+      response = "Thank you.";
+      break;
+
+      case 'thanks for everything':
+      response = "Bye.";
+      break;  
+      
+  }
+
+  return response;
+
 }
